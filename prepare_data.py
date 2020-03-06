75fbf96308191a9d6cf894f04deb873374bcde43
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-03-06 15:56:32 +0100

    Add more prints

--- prepare_data.py
+++ prepare_data.py
@@ -7,11 +7,14 @@ model_name = "tweets.wv"
 def prepare_data(cursor, train=True):
     # Todo use the database text here
     if train:
+        print("Training word2vec model")
         model = Word2Vec(common_texts, size=100, window=5, min_count=1, workers=4)
         model.wv.save(model_name)
         model.save(model_name + '.model')
+    else:
+        print("Skip training new word2vec model")
 
     wv = KeyedVectors.load(model_name, mmap='r')
     computer_vec = wv['computer']
 
-    return 1, 2
+    return 1, 2=
