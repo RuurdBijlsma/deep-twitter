442079ef5e2c3ae7429b81ce69a872dbf5157deb
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-03-17 21:03:20 +0100

    Fix remove tweets, fix tweet limit

--- csv_processor.py
+++ csv_processor.py
@@ -3,14 +3,30 @@ import csv
 
 def remove_url(tweet):
     parts = tweet.split(' ')
-    url_index = -1
+    url_indices = []
     for (i, part) in enumerate(parts):
-        if any(tld in part for tld in tlds):
-            url_index = i
-    if url_index == -1:
+        for tld in tlds:
+            if tld in part:
+                sub_index = part.find(tld)
+                next_letter_index = sub_index + len(tld)
+                if next_letter_index < len(part):
+                    next_letter = part[next_letter_index]
+                    if next_letter == '.':
+                        continue
+                    if next_letter.isalpha():
+                        continue
+                if sub_index - 1 >= 0:
+                    prev_letter = part[sub_index - 1]
+                    if not prev_letter.isalpha():
+                        continue
+
+                url_indices.append(i)
+                break
+    if len(url_indices) == 0:
         return tweet
     else:
-        parts.pop(url_index)
+        for url_index in sorted(url_indices, reverse=True):
+            del parts[url_index]
         return ' '.join(parts)
 
 
@@ -32,6 +48,10 @@ def import_csv_to_db(cursor, csv_file_name="data/training_tweets.csv"):
         for [_, _, _, _, user, text] in data_reader:
             line += 1
             cleaned = remove_url(text)
-            cursor.execute('INSERT INTO tweets VALUES(?,?,?,?)', (None, text, cleaned, user))
+            cleaned = cleaned.strip()
+            if cleaned != '':
+                cursor.execute('INSERT INTO tweets VALUES(?,?,?,?)', (None, text, cleaned, user))
+            else:
+                print(f"SKIPPED TWEET: {text}")
             if line % 5000 == 0:
                 print(f"Tweets processed: [{int(line / 1000)}k / {int(total_tweets / 1000)}k]")
